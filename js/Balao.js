b1b36cae09159c430869ea9655dca78bb6bd447b
Fábio Veiga <veigabfabio@hotmail.com>
2023-06-22 21:47:44 -0200

    Texto de dialogo front-end

--- js/Balao.js
+++ js/Balao.js
@@ -1,6 +1,23 @@
-class Balao{
+class Balao {
     static balao = document.querySelector(".texto")
-    static aparecer(){
+    static falas = `{
+        "textos": [
+            {
+                "indice": "0",
+                "texto": "Olá! Seja bem-vindo ao DriveGuide. Se você veio em busca de informações sobre cloud computing e mais precisamente sobre Google Drive veio ao lugar certo!"
+            },
+            {
+                "indice": "1",
+                "texto": "monitor"
+            }
+        ]
+    }`
+    static aparecer() {
         this.balao.style.display = 'initial'
+        Balao.digitar()
+    }
+
+    static digitar(fala) {
+        console.log(JSON.parse(this.falas).textos[0].texto)
     }
 }
